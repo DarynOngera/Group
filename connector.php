137a3b21f3c3901edfdbd2bdca415b37b1a4d2cf
DarynOngera <ongeradaryn@gmail.com>
2024-11-12 20:10:57 +0300

    n

--- connector.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-mysqli_real_connect($conn, "grouo-server.mysql.database.azure.com", "ServerAdmin", "ServerAdmin123", "grouo-database", 3306, MYSQLI_CLIENT_SSL);
